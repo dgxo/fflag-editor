5985ff470cb2ce1f9f5798199b3668a737e2cac4
Dog <104234930+dgxo@users.noreply.github.com>
2025-08-14 05:29:44 +0100

    Add padding

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="bg-gray-900 flex flex-col gap-0.5 min-h-screen text-gray-100">
+	<div
+		class="bg-gray-900 flex flex-col p-5 gap-0.5 min-h-screen text-gray-100"
+	>
 		<!-- <LoggedInHeader /> -->
 		<!-- <div class="bg-zinc-900 flex flex-col gap-2 flex-grow"> -->
 		<slot />
